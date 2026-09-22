60a3bde2962b49e9f93f969c53133a57a1eaa733
eukyre <130890675+eukyre@users.noreply.github.com>
2026-09-22 22:42:16 +0100

    Dependancy Fix

--- EukyreECOT/EukyreMod.cs
+++ EukyreECOT/EukyreMod.cs
@@ -18,8 +18,8 @@ public record ModMetadata : AbstractModMetadata
     public override List<string>? Incompatibilities { get; init; }
     public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
     {
-        { "com.wtt.commonlib", new Range("~2.0.20") },
-        { "com.epicrangetime.aio", new Range("~4.0.0") }
+        { "com.wtt.commonlib", new Range("^2.0.20") },
+        { "com.epicrangetime.aio", new Range("^4.0.0") }
     };
     public override string? Url { get; init; }
     public override bool? IsBundleMod { get; init; } = true;
